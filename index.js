e5619b709296cbb8ca086f95392851fe26125f9e
nocturnalbeast <nocturnalbeast@github.com>
2022-11-03 19:54:15 +0530

    Fixes for index.js:  * Change vars to const  * Fix for run failure on partials dir missing  * Some refactoring/cleanup via prettier

--- index.js
+++ index.js
@@ -1,116 +1,123 @@
-var fs = require("fs");
-var path = require("path");
-var Handlebars = require("handlebars");
+const fs = require("fs");
+const path = require("path");
+const Handlebars = require("handlebars");
 
-Handlebars.registerHelper("join", function (array, sep, options) {
-    return array
-        .map(function (item) {
-            return item;
-        })
-        .join(sep);
+Handlebars.registerHelper("join", function (array, sep) {
+  return array
+    .map(function (item) {
+      return item;
+    })
+    .join(sep);
 });
 
 function render(resume) {
-    if (resume.work && resume.work.length) {
-        resume.workEnabled = true;
+  if (resume.work && resume.work.length) {
+    resume.workEnabled = true;
+  } else {
+    resume.workEnabled = false;
+  }
+
+  if (resume.volunteer && resume.volunteer.length) {
+    resume.volunteerEnabled = true;
+  } else {
+    resume.volunteerEnabled = false;
+  }
+
+  if (resume.projects && resume.projects.length) {
+    resume.projectsEnabled = true;
+  } else {
+    resume.projectsEnabled = false;
+  }
+
+  if (resume.education && resume.education.length) {
+    resume.educationEnabled = true;
+  } else {
+    resume.educationEnabled = false;
+  }
+
+  if (resume.awards && resume.awards.length) {
+    resume.awardsEnabled = true;
+  } else {
+    resume.awardsEnabled = false;
+  }
+
+  if (resume.publications && resume.publications.length) {
+    resume.publicationsEnabled = true;
+  } else {
+    resume.publicationsEnabled = false;
+  }
+
+  if (resume.skills && resume.skills.length) {
+    resume.skillsEnabled = true;
+  } else {
+    resume.skillsEnabled = false;
+  }
+
+  if (resume.interests && resume.interests.length) {
+    resume.interestsEnabled = true;
+  } else {
+    resume.interestsEnabled = false;
+  }
+
+  if (resume.languages && resume.languages.length) {
+    resume.languagesEnabled = true;
+  } else {
+    resume.languagesEnabled = false;
+  }
+
+  if (resume.references && resume.references.length) {
+    resume.referencesEnabled = true;
+  } else {
+    resume.referencesEnabled = false;
+  }
+
+  // use an ugly hack to set PDF/export-related options
+  if (process.argv.slice(2)[0] !== "export") {
+    const exportToPDF = {
+      network: "Export to PDF",
+      username: "",
+      url: "javascript:window.print();",
+    };
+
+    if (resume.basics.profiles && resume.basics.profiles.length) {
+      resume.basics.profiles[resume.basics.profiles.length] = exportToPDF;
     } else {
-        resume.workEnabled = false;
+      resume.basics.profiles[0] = exportToPDF;
     }
+    resume.linksEnabled = true;
+  } else {
+    resume.linksEnabled = false;
+  }
 
-    if (resume.volunteer && resume.volunteer.length) {
-        resume.volunteerEnabled = true;
-    } else {
-        resume.volunteerEnabled = false;
-    }
+  const css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+  const tpl = fs.readFileSync(__dirname + "/main.hbs", "utf-8");
 
-    if (resume.projects && resume.projects.length) {
-        resume.projectsEnabled = true;
-    } else {
-        resume.projectsEnabled = false;
-    }
+  const partialsDir = path.join(__dirname, "partials");
 
-    if (resume.education && resume.education.length) {
-        resume.educationEnabled = true;
-    } else {
-        resume.educationEnabled = false;
-    }
+  if (fs.existsSync(partialsDir)) {
+    const filenames = fs.readdirSync(partialsDir);
 
-    if (resume.awards && resume.awards.length) {
-        resume.awardsEnabled = true;
-    } else {
-        resume.awardsEnabled = false;
-    }
-
-    if (resume.publications && resume.publications.length) {
-        resume.publicationsEnabled = true;
-    } else {
-        resume.publicationsEnabled = false;
-    }
-
-    if (resume.skills && resume.skills.length) {
-        resume.skillsEnabled = true;
-    } else {
-        resume.skillsEnabled = false;
-    }
-
-    if (resume.interests && resume.interests.length) {
-        resume.interestsEnabled = true;
-    } else {
-        resume.interestsEnabled = false;
-    }
-
-    if (resume.languages && resume.languages.length) {
-        resume.languagesEnabled = true;
-    } else {
-        resume.languagesEnabled = false;
-    }
-
-    if (resume.references && resume.references.length) {
-        resume.referencesEnabled = true;
-    } else {
-        resume.referencesEnabled = false;
-    }
-
-    // use an ugly hack to set PDF/export-related options
-    if (process.argv.slice(2)[0] != "export") {
-        const exportToPDF = {
-            network: "Export to PDF",
-            username: "",
-            url: "javascript:window.print();",
-        };
-        if (resume.basics.profiles && resume.basics.profiles.length) {
-            resume.basics.profiles[resume.basics.profiles.length] = exportToPDF;
-        } else {
-            resume.basics.profiles[0] = exportToPDF;
-        }
-        resume.linksEnabled = true;
-    } else {
-        resume.linksEnabled = false;
-    }
-
-    var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-    var tpl = fs.readFileSync(__dirname + "/main.hbs", "utf-8");
-
-    var partialsDir = path.join(__dirname, "partials");
-    var filenames = fs.readdirSync(partialsDir);
     filenames.forEach(function (filename) {
-        var matches = /^([^.]+).hbs$/.exec(filename);
-        if (!matches) {
-            return;
-        }
-        var name = matches[1];
-        var filepath = path.join(partialsDir, filename);
-        var template = fs.readFileSync(filepath, "utf8");
+      const matches = /^([^.]+).hbs$/.exec(filename);
 
-        Handlebars.registerPartial(name, template);
-    });
+      if (!matches) {
+        return;
+      }
 
-    return Handlebars.compile(tpl)({
-        css: css,
-        resume: resume,
+      const name = matches[1];
+      const filepath = path.join(partialsDir, filename);
+      const template = fs.readFileSync(filepath, "utf8");
+
+      Handlebars.registerPartial(name, template);
     });
+  }
+
+  return Handlebars.compile(tpl)({
+    css,
+    resume,
+  });
 }
 
 module.exports = {
-    render: render,
+  render,
 };
